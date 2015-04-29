2eeac6af92ef21d15f2384436c3242f470376556
Brian <brian.kropff@gmail.com>
2015-04-29 14:07:50 -0700

    Add heroku files

--- app/app.php
+++ app/app.php
@@ -6,8 +6,16 @@
 
     $app['debug'] = true;
 
-    $DB = new PDO('pgsql:host=localhost;dbname=shoes;');
-
+    $dbopts = parse_url(getenv('DATABASE_URL'));
+    $app->register(new Herrera\Pdo\PdoServiceProvider(),
+      array(
+        'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
+        'pdo.port' => $dbopts["port"],
+        'pdo.username' => $dbopts["user"],
+        'pdo.password' => $dbopts["pass"]
+      )
+    );
+    $DB= $app['pdo'];
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
